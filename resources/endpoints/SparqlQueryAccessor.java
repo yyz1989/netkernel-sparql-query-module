f946a6487275ca3829ee6199780e319cd4907155
Yang Yuanzhe <yyz1989@hotmail.com>
2015-03-23 13:48:29 +0100

    Renamed accessor class and added code comments

--- resources/endpoints/SparqlQueryAccessor.java
+++ resources/endpoints/SparqlQueryAccessor.java
@@ -1,19 +1,29 @@
 package resources.endpoints;
-
-import org.netkernel.client.http.representation.HttpClientResponseRepresentation;
+/**
+ * A SPARQL query client for Netkernel
+ * @author Proxml
+ */
 import org.netkernel.layer0.nkf.*;
 import org.netkernel.layer0.representation.IHDSNode;
 import org.netkernel.layer0.representation.impl.HDSBuilder;
 import org.netkernel.module.standard.endpoint.StandardAccessorImpl;
 
-
-public class SparqlQueryExecutor extends StandardAccessorImpl
+/**
+ * A Netkernel accessor handling the incoming SPARQL query requests
+ */
+public class SparqlQueryAccessor extends StandardAccessorImpl
 {
-
+    /**
+     * Reaction of a Netkernel SOURCE request issued from the HTTP frontend
+     * @param context the context where the Netkernel request comes from
+     * @throws Exception
+     */
     public void onSource(INKFRequestContext context) throws Exception
     {
         INKFResponseReadOnly response;
-        INKFRequest httpRequest;
+        INKFRequest request;
+
+        // Get query arguments
         String httpMethod = context.source("httpRequest:/method", String.class);
         IHDSNode headers = context.source("httpRequest:/headers", IHDSNode.class);
         IHDSNode connection = context.source("arg:connection", IHDSNode.class);
@@ -21,41 +31,49 @@ public class SparqlQueryExecutor extends StandardAccessorImpl
         String port = connection.getFirstValue("//port").toString();
         String dataset = connection.getFirstValue("//dataset").toString();
         String operation = connection.getFirstValue("//operation").toString();
+
+        // URL of the SPARQL endpoint to be accessed
         String endpoint = host + ":" + port + "/" + dataset + "/" + operation;
         String query;
 
         if (httpMethod.equals("GET")) {
             query = context.source("httpRequest:/param/query", String.class);
-            httpRequest = context.createRequest("active:httpGet");
-            httpRequest.setVerb(INKFRequestReadOnly.VERB_SOURCE);
-            httpRequest.addArgument("url", endpoint + "?query=" + query);
-            httpRequest.addArgumentByValue("headers", headers.getRoot());
+            request = context.createRequest("active:httpGet");
+            request.setVerb(INKFRequestReadOnly.VERB_SOURCE);
+            request.addArgument("url", endpoint + "?query=" + query);
+            request.addArgumentByValue("headers", headers.getRoot());
         }
         else if (httpMethod.equals("POST")) {
+            // The body of POST must be URLencoded form data represented in HDS format
             query = context.source("httpRequest:/postparam/query", String.class);
             HDSBuilder body = new HDSBuilder();
             body.pushNode("query", query);
+
+            // The headers of POST must be name-value pairs in HDS format, which must be
+            // rebuilt to avoid conflicts generated during relay
             HDSBuilder newHeaders = new HDSBuilder();
             newHeaders.addNode("Accept-Encoding", headers.getFirstValue("//Accept-Encoding"));
             newHeaders.addNode("Accept", headers.getFirstValue("//Accept"));
             newHeaders.addNode("Accept-Language", headers.getFirstValue("//Accept-Language"));
-            httpRequest = context.createRequest("active:httpPost");
-            httpRequest.setVerb(INKFRequestReadOnly.VERB_SOURCE);
-            httpRequest.addArgument("url", endpoint);
-            httpRequest.addArgumentByValue("nvp", body.getRoot());
-            httpRequest.addArgumentByValue("headers", newHeaders.getRoot());
+
+            request = context.createRequest("active:httpPost");
+            request.setVerb(INKFRequestReadOnly.VERB_SOURCE);
+            request.addArgument("url", endpoint);
+            request.addArgumentByValue("nvp", body.getRoot());
+            request.addArgumentByValue("headers", newHeaders.getRoot());
         }
         else {
-            httpRequest = context.createRequest("access-blocked");
+            // Deny services for other type of HTTP requests
+            request = context.createRequest("access-blocked");
             query = "Request is not acceptable";
         }
 
         context.logRaw(
                 INKFRequestContext.LEVEL_INFO,
-                "Received SPARQL Query " + httpMethod + " Request :" + query
+                "Received SPARQL Query " + httpMethod + " Request: " + query
         );
 
-        response = context.issueRequestForResponse(httpRequest);
+        response = context.issueRequestForResponse(request);
         context.createResponseFrom(response);
     }
 }
