f374b99f507b5c6ade03707fae4597119410a965
Yang Yuanzhe <yyz1989@hotmail.com>
2015-03-23 01:26:56 +0100

    Supported POST request

--- resources/endpoints/SparqlQueryExecutor.java
+++ resources/endpoints/SparqlQueryExecutor.java
@@ -1,6 +1,8 @@
 package resources.endpoints;
 
 import org.netkernel.layer0.nkf.*;
+import org.netkernel.layer0.representation.IHDSNode;
+import org.netkernel.layer0.representation.impl.HDSBuilder;
 import org.netkernel.module.standard.endpoint.StandardAccessorImpl;
 
 
@@ -11,29 +13,36 @@ public class SparqlQueryExecutor extends StandardAccessorImpl
     {
         INKFRequest httpRequest;
         Object httpResponse;
-        String path = context.source("httpRequest:/url", String.class);
         String httpMethod = context.source("httpRequest:/method", String.class);
-        String mimeType = context.source("httpRequest:/accept/preferred", String.class);
-        String query = context.source("httpRequest:/param/query", String.class);
-        context.logRaw(
-                INKFRequestContext.LEVEL_INFO,
-                "Received SPARQL Query Request " + query
-        );
+        IHDSNode acceptHeaders = context.source("httpRequest:/accept", IHDSNode.class);
+        String query = "";
 
         if (httpMethod.equals("GET")) {
+            query = context.source("httpRequest:/param/query", String.class);
             httpRequest = context.createRequest("active:httpGet");
             httpRequest.setVerb(INKFRequestReadOnly.VERB_SOURCE);
             httpRequest.addArgument("url", "http://localhost:3030/tdb/query?query=" + query);
+            httpRequest.addArgumentByValue("headers", acceptHeaders.getRoot());
         }
         else if (httpMethod.equals("POST")) {
+            query = context.source("httpRequest:/postparam/query", String.class);
+            HDSBuilder body = new HDSBuilder();
+            body.pushNode("query", query);
             httpRequest = context.createRequest("active:httpPost");
-            httpRequest.setVerb(INKFRequestReadOnly.VERB_SINK);
+            httpRequest.setVerb(INKFRequestReadOnly.VERB_SOURCE);
             httpRequest.addArgument("url", "http://localhost:3030/tdb/query");
-            httpRequest.addArgument("query", query);
+            httpRequest.addArgumentByValue("nvp", body.getRoot());
+            httpRequest.addArgumentByValue("headers", acceptHeaders.getRoot());
         }
         else {
             httpRequest = context.createRequest("access-blocked");
+            query = "Query is invalid";
         }
+
+        context.logRaw(
+                INKFRequestContext.LEVEL_INFO,
+                "Received SPARQL Query " + httpMethod + " Request :" + query
+        );
 
         httpResponse = context.issueRequest(httpRequest);
         context.createResponseFrom(httpResponse);
