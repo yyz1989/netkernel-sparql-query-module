4d5f2a93f372608baaae1fc84cf70c88eab0ce58
Yang Yuanzhe <yyz1989@hotmail.com>
2015-03-23 13:06:01 +0100

    Relayed request headers, moved connection config into module file

--- resources/endpoints/SparqlQueryExecutor.java
+++ resources/endpoints/SparqlQueryExecutor.java
@@ -1,5 +1,6 @@
 package resources.endpoints;
 
+import org.netkernel.client.http.representation.HttpClientResponseRepresentation;
 import org.netkernel.layer0.nkf.*;
 import org.netkernel.layer0.representation.IHDSNode;
 import org.netkernel.layer0.representation.impl.HDSBuilder;
@@ -11,32 +12,42 @@ public class SparqlQueryExecutor extends StandardAccessorImpl
 
     public void onSource(INKFRequestContext context) throws Exception
     {
+        INKFResponseReadOnly response;
         INKFRequest httpRequest;
-        Object httpResponse;
         String httpMethod = context.source("httpRequest:/method", String.class);
-        IHDSNode acceptHeaders = context.source("httpRequest:/accept", IHDSNode.class);
-        String query = "";
+        IHDSNode headers = context.source("httpRequest:/headers", IHDSNode.class);
+        IHDSNode connection = context.source("arg:connection", IHDSNode.class);
+        String host = connection.getFirstValue("//host").toString();
+        String port = connection.getFirstValue("//port").toString();
+        String dataset = connection.getFirstValue("//dataset").toString();
+        String operation = connection.getFirstValue("//operation").toString();
+        String endpoint = host + ":" + port + "/" + dataset + "/" + operation;
+        String query;
 
         if (httpMethod.equals("GET")) {
             query = context.source("httpRequest:/param/query", String.class);
             httpRequest = context.createRequest("active:httpGet");
             httpRequest.setVerb(INKFRequestReadOnly.VERB_SOURCE);
-            httpRequest.addArgument("url", "http://localhost:3030/tdb/query?query=" + query);
-            httpRequest.addArgumentByValue("headers", acceptHeaders.getRoot());
+            httpRequest.addArgument("url", endpoint + "?query=" + query);
+            httpRequest.addArgumentByValue("headers", headers.getRoot());
         }
         else if (httpMethod.equals("POST")) {
             query = context.source("httpRequest:/postparam/query", String.class);
             HDSBuilder body = new HDSBuilder();
             body.pushNode("query", query);
+            HDSBuilder newHeaders = new HDSBuilder();
+            newHeaders.addNode("Accept-Encoding", headers.getFirstValue("//Accept-Encoding"));
+            newHeaders.addNode("Accept", headers.getFirstValue("//Accept"));
+            newHeaders.addNode("Accept-Language", headers.getFirstValue("//Accept-Language"));
             httpRequest = context.createRequest("active:httpPost");
             httpRequest.setVerb(INKFRequestReadOnly.VERB_SOURCE);
-            httpRequest.addArgument("url", "http://localhost:3030/tdb/query");
+            httpRequest.addArgument("url", endpoint);
             httpRequest.addArgumentByValue("nvp", body.getRoot());
-            httpRequest.addArgumentByValue("headers", acceptHeaders.getRoot());
+            httpRequest.addArgumentByValue("headers", newHeaders.getRoot());
         }
         else {
             httpRequest = context.createRequest("access-blocked");
-            query = "Query is invalid";
+            query = "Request is not acceptable";
         }
 
         context.logRaw(
@@ -44,7 +55,7 @@ public class SparqlQueryExecutor extends StandardAccessorImpl
                 "Received SPARQL Query " + httpMethod + " Request :" + query
         );
 
-        httpResponse = context.issueRequest(httpRequest);
-        context.createResponseFrom(httpResponse);
+        response = context.issueRequestForResponse(httpRequest);
+        context.createResponseFrom(response);
     }
 }
